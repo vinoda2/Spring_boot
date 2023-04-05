646ee167d3ad906c8cf71de77a135842c1f0bb82
vinoda2 <109263358+vinoda2@users.noreply.github.com>
2023-04-05 16:24:46 +0530

    Update StudentDTO.java

--- googlesheetconnection/src/main/java/com/xworkz/googlesheetconnection/dto/StudentDTO.java
+++ googlesheetconnection/src/main/java/com/xworkz/googlesheetconnection/dto/StudentDTO.java
@@ -1,16 +1,48 @@
 package com.xworkz.googlesheetconnection.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
 public class StudentDTO {
 
+	
 	private String studentName;
 	private String email;
 	private String contactNumber;
 	private String address;
+	private Boolean disabled=false;
+	
+	public Boolean getDisabled() {
+		return disabled;
+	}
+	public void setDisabled(Boolean disabled) {
+		this.disabled = disabled;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "StudentDTO [studentName=" + studentName + ", email=" + email + ", contactNumber=" + contactNumber
+				+ ", address=" + address + "]";
+	}
 }
