cc23e21756f031339222ddcfe65f5030e27efd95
vinoda2 <109263358+vinoda2@users.noreply.github.com>
2023-04-05 16:25:56 +0530

    Update SheetService.java

--- googlesheetconnection/src/main/java/com/xworkz/googlesheetconnection/service/SheetService.java
+++ googlesheetconnection/src/main/java/com/xworkz/googlesheetconnection/service/SheetService.java
@@ -16,16 +16,6 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.AppendValuesResponse;
-import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetRequest;
-import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetResponse;
-import com.google.api.services.sheets.v4.model.ClearValuesRequest;
-import com.google.api.services.sheets.v4.model.ClearValuesResponse;
-import com.google.api.services.sheets.v4.model.FindReplaceRequest;
-import com.google.api.services.sheets.v4.model.FindReplaceResponse;
-import com.google.api.services.sheets.v4.model.Request;
-import com.google.api.services.sheets.v4.model.Spreadsheet;
-import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
-import com.google.api.services.sheets.v4.model.UpdateSpreadsheetPropertiesRequest;
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import com.xworkz.googlesheetconnection.dto.StudentDTO;
@@ -37,16 +27,9 @@ public class SheetService {
 	private static final String APPLICATION_NAME = "Google sheet new ";
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 	private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-	private static final String CREDENTIALS_FILE_PATH = "F:\\x-workz\\googlesheetconnection\\src\\main\\resources\\credentials.json";
+	private static final String CREDENTIALS_FILE_PATH = "C:\\Users\\Dell\\Desktop\\New folder\\x-workz\\googlesheetconnection\\src\\main\\resources\\credentials.json";
 	private static Sheets sheet;
-
-	int startRow = 2;
-	int endRow = 50;
-	int startColumn = 1;
-	int endColumn = 4;
-	String range = String.format("Sheet1!%s%d:%s%d", GoogleSheetUtil.getColumnLetter(startColumn), startRow,
-			GoogleSheetUtil.getColumnLetter(endColumn), endRow);
-
+	String range = "A2:E";
 	GoogleCredential credential;
 
 	// loading the connection
@@ -60,135 +43,213 @@ public class SheetService {
 		System.err.println("Base url:" + sheet.getBaseUrl());
 	}
 
-	// creating new google sheet but we are not using it
-	public static String createSheet(String sheetName) throws IOException {
-		Spreadsheet spreadsheet = new Spreadsheet().setProperties(new SpreadsheetProperties().setTitle(sheetName));
-		spreadsheet = sheet.spreadsheets().create(spreadsheet).setFields("spreadsheetId").execute();
-		// Prints the new spreadsheet id
-		System.out.println("Spreadsheet ID: " + spreadsheet.getSpreadsheetId());
-		return spreadsheet.getSpreadsheetId();
-	}
-
 	// inserting data into Google Sheet
-	public AppendValuesResponse writeData(String SheetId, StudentDTO dto) {
-
-		AppendValuesResponse writeResult = null;
-		String valueInputOption = "Raw";
+	public String writeData(String SheetId, StudentDTO dto) {
 		List<List<Object>> objectlist = new ArrayList<>();
-
 		List<Object> list = new ArrayList<>();
-
-		Object name = dto.getStudentName();
-		Object email = dto.getEmail();
-		Object contactNumbet = dto.getContactNumber();
-		Object address = dto.getAddress();
-		list.add(name);
-		list.add(email);
-		list.add(contactNumbet);
-		list.add(address);
+		list.add(dto.getStudentName());
+		list.add(dto.getEmail());
+		list.add(dto.getContactNumber());
+		list.add(dto.getAddress());
+		list.add(dto.getDisabled());
 		objectlist.add(list);
-		System.out.println(objectlist);
 		ValueRange body = new ValueRange().setValues(objectlist);
 		try {
-			writeResult = sheet.spreadsheets().values().append(SheetId, range, body)
-					.setValueInputOption(valueInputOption).execute();
+			AppendValuesResponse writeResult = sheet.spreadsheets().values().append(SheetId, range, body)
+					.setValueInputOption("Raw").execute();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return writeResult;
+		return "Data saved Successfully";
 
 	}
 
 	// read single value from Google Sheet
 	public List<StudentDTO> getValues(String sheetId) throws IOException {
-		System.err.println("Read method");
-		ValueRange result = null;
-		result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
 		List<List<Object>> list = result.getValues();
 		List<StudentDTO> sdto = new ArrayList<StudentDTO>();
-		for (List<Object> value : list) {
-			StudentDTO dto = new StudentDTO();
-			Object name = value.get(0);
-			Object email = value.get(1);
-			Object contactNumber = value.get(2);
-			Object address = value.get(3);
-			dto.setStudentName((String) name);
-			dto.setEmail((String) email);
-			dto.setContactNumber((String) contactNumber);
-			dto.setAddress((String) address);
-			sdto.add(dto);
+		if (list != null && list.size() != 0) {
+			for (List<Object> value : list) {
+				StudentDTO dto = new StudentDTO();
+				dto.setStudentName((String) value.get(0));
+				dto.setEmail((String) value.get(1));
+				dto.setContactNumber((String) value.get(2));
+				dto.setAddress((String) value.get(3));
+				String status = (String) value.get(4);
+				Boolean statusconvert = Boolean.valueOf(status);
+				dto.setDisabled(statusconvert);
+				sdto.add(dto);
+			}
+
+			return sdto;
+		} else {
+			System.out.println("Sheet is empty");
 		}
-		return sdto;
+		return null;
 	}
-	//update with range
-	public UpdateValuesResponse updateData(String SheetId, StudentDTO dto, String range) {
-		UpdateValuesResponse updateResponse = null;
-		String valueInputOption = "Raw";
-		List<List<Object>> objectlist = new ArrayList<>();
 
-		List<Object> list = new ArrayList<>();
-
-		Object name = dto.getStudentName();
-		Object email = dto.getEmail();
-		Object contactNumbet = dto.getContactNumber();
-		Object address = dto.getAddress();
-		list.add(name);
-		list.add(email);
-		list.add(contactNumbet);
-		list.add(address);
-		objectlist.add(list);
-		System.out.println(objectlist);
-		ValueRange body = new ValueRange().setValues(objectlist);
-		try {
-			updateResponse = sheet.spreadsheets().values().update(SheetId, range, body)
-					.setValueInputOption(valueInputOption).execute();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	// update by properties
+	public String listUpdate(String sheetId, StudentDTO dto) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		int updateIndex = 2;
+		for (List<Object> list : valueList) {
+			if (list.get(0).equals(dto.getStudentName())) {
+				String columnLetter = GoogleSheetUtil.getColumnLetter(1) + updateIndex;
+				String updateRange = columnLetter + ":D";
+				List<Object> updateList = new ArrayList<Object>();
+				updateList.add(dto.getStudentName());
+				updateList.add(dto.getEmail());
+				updateList.add(dto.getContactNumber());
+				updateList.add(dto.getAddress());
+				List<List<Object>> lists = new ArrayList();
+				lists.add(updateList);
+				ValueRange body = new ValueRange().setValues(lists);
+				UpdateValuesResponse updateResponse = sheet.spreadsheets().values().update(sheetId, updateRange, body)
+						.setValueInputOption("Raw").execute();
+				return "update Successfully done";
+			}
+			updateIndex++;
 		}
-		return updateResponse;
-
+		return "update failed";
 	}
 
-	// updating or renaming the google sheet
-	public BatchUpdateSpreadsheetResponse update(String spreadsheetId, String title) throws IOException {
-		List<Request> requests = new ArrayList<>();
-		BatchUpdateSpreadsheetResponse response = null;
-		// Changing the spread sheet title
-		requests.add(new Request().setUpdateSpreadsheetProperties(new UpdateSpreadsheetPropertiesRequest()
-				.setProperties(new SpreadsheetProperties().setTitle(title)).setFields("title")));
-		BatchUpdateSpreadsheetRequest body = new BatchUpdateSpreadsheetRequest().setRequests(requests);
-		response = sheet.spreadsheets().batchUpdate(spreadsheetId, body).execute();
-		return response;
+	// find by Name return list<Object>
+	public List<StudentDTO> findByName(String sheetId, String name) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		List<StudentDTO> nameList = new ArrayList<StudentDTO>();
+
+		if (valueList != null && valueList.size() != 0) {
+			for (List<Object> list : valueList) {
+				if (list.get(0).equals(name)) {
+					StudentDTO dto = new StudentDTO();
+					dto.setStudentName((String) list.get(0));
+					dto.setEmail((String) list.get(1));
+					dto.setContactNumber((String) list.get(2));
+					dto.setAddress((String) list.get(3));
+					nameList.add(dto);
+				} else {
+					System.out.println("Name not present");
+				}
+			}
+
+		}
+		return nameList;
 	}
 
-	//it's not working properly check once
-	public BatchUpdateSpreadsheetResponse updateByName(String sheetId, String name, String replacement)
-			throws IOException {
+	// Find By Address return List<Object>
+	public List<StudentDTO> findByAddress(String sheetId, String address) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		List<StudentDTO> addressList = new ArrayList<StudentDTO>();
 
-		List<Request> requests = new ArrayList<>();
-		BatchUpdateSpreadsheetResponse response = null;
-		// Find and replace text.
-		requests.add(new Request()
-				.setFindReplace(new FindReplaceRequest().setFind(name).setReplacement(replacement).setAllSheets(true)));
-
-		BatchUpdateSpreadsheetRequest body = new BatchUpdateSpreadsheetRequest().setRequests(requests);
-		response = sheet.spreadsheets().batchUpdate(sheetId, body).execute();
-		FindReplaceResponse findReplaceResponse = response.getReplies().get(2).getFindReplace();
-		return response;
-
+		if (valueList != null && valueList.size() != 0) {
+			for (List<Object> list : valueList) {
+				if (list.get(3).equals(address)) {
+					StudentDTO dto = new StudentDTO();
+					dto.setStudentName((String) list.get(0));
+					dto.setEmail((String) list.get(1));
+					dto.setContactNumber((String) list.get(2));
+					dto.setAddress((String) list.get(3));
+					addressList.add(dto);
+				}
+			}
+		}
+		return addressList;
 	}
 
-	//deletion with range
-	public String delete(String sheetId, String range) throws IOException {
-		System.err.println("delete method");
-		// Assign values to desired fields of `requestBody`:
-		ClearValuesRequest requestBody = new ClearValuesRequest();
-		Sheets.Spreadsheets.Values.Clear request = sheet.spreadsheets().values().clear(sheetId, range, requestBody);
-		ClearValuesResponse response = request.execute();
+	// Find By Email it returns DTO matching with Email
+	public StudentDTO findByEmail(String sheetId, String email) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		StudentDTO dto = new StudentDTO();
+		if (valueList != null && valueList.size() != 0) {
+			for (List<Object> list : valueList) {
+				if (list.get(1).equals(email)) {
+					dto.setStudentName((String) list.get(0));
+					dto.setEmail((String) list.get(1));
+					dto.setContactNumber((String) list.get(2));
+					dto.setAddress((String) list.get(3));
+				}
+			}
+		}
+		return dto;
+	}
 
-		return "cell cleared";
+	// Find by Mobile number it return DTO matching with Mobile number
+	public StudentDTO findByMobileNumber(String sheetId, String mobileNumber) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		StudentDTO dto = new StudentDTO();
+		if (valueList != null && valueList.size() != 0) {
+			for (List<Object> list : valueList) {
+				if (list.get(2).equals(mobileNumber)) {
+					dto.setStudentName((String) list.get(0));
+					dto.setEmail((String) list.get(1));
+					dto.setContactNumber((String) list.get(2));
+					dto.setAddress((String) list.get(3));
+				} else {
+					System.out.println("Mobile number Not found");
+				}
+			}
+		}
+		return dto;
+	}
+
+	// updateDisableByEmail
+	public String updateDisableByEmail(String sheetId, String email) throws IOException {
+		range = "A2:E";
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		int updateIndex = 2;
+		if (valueList != null && valueList.size() != 0) {
+			for (List<Object> list : valueList) {
+				String updateRange = GoogleSheetUtil.getColumnLetter(5) + updateIndex;
+				if (list.get(1).equals(email)) {
+					Object disableStatus = list.get(4);
+					if (!disableStatus.equals("TRUE")) {
+						disableStatus = "TRUE";
+						List<Object> objlist = new ArrayList<Object>();
+						objlist.add(disableStatus);
+						List<List<Object>> updateList = new ArrayList<>();
+						updateList.add(objlist);
+						ValueRange body = new ValueRange().setValues(updateList);
+						UpdateValuesResponse updateResponse = sheet.spreadsheets().values()
+								.update(sheetId, updateRange, body).setValueInputOption("Raw").execute();
+					} else {
+						return "Account Already disabled";
+					}
+				}
+				updateIndex++;
+			}
+		} else {
+			return "Data not Exists";
+		}
+		return "Successfully Disabled";
+	}
+
+	// find By active students
+	public List<StudentDTO> findByEnabled(String sheetId) throws IOException {
+		ValueRange result = sheet.spreadsheets().values().get(sheetId, range).execute();
+		List<List<Object>> valueList = result.getValues();
+		List<StudentDTO> activeStudent = new ArrayList<StudentDTO>();
+		for (List<Object> list : valueList) {
+			String disableStatus = (String) list.get(4);
+			Boolean activeStatus = Boolean.valueOf(disableStatus);
+			if (!activeStatus.equals(true)) {
+				StudentDTO dto = new StudentDTO();
+				dto.setStudentName((String) list.get(0));
+				dto.setEmail((String) list.get(1));
+				dto.setContactNumber((String) list.get(2));
+				dto.setAddress((String) list.get(3));
+				activeStudent.add(dto);
+			} else {
+				System.out.println("account disabled");
+			}
+		}
+		return activeStudent;
 	}
 
 }
